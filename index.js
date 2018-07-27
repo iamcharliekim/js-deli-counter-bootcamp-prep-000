91ccbe55b851241ca340535758d8e8e7b4e6b86b
Charles Kim <cwkim3@gmail.com>
2018-07-27 00:33:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber (katzDeliLine,name){
   for (let i = 0; i < katzDeliLine.length; i++)
   katzDeliLine.push(name);
+}
 return `Welcome, ${newPersonsName[i]}. You are number ${katzDeliLine[i]
 }`;
-}
