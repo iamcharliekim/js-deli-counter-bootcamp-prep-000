322903d5cfa1b97f32ccd516c3d3bdcc0503e3de
Charles Kim <cwkim3@gmail.com>
2018-07-27 00:24:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function takeANumber(katzDeliLine, name){
+function takeANumber (katzDeliLine, name){
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
-  }
+}
 
-function nowServing(katzDeliLine){
+function nowServing (katzDeliLine){
   if (katzDeliLine.length === 0){
     return "There is nobody waiting to be served!";
   }
@@ -12,15 +12,13 @@ function nowServing(katzDeliLine){
   }
 }
 
-function currentLine(katzDeliLine){
+function currentLine (katzDeliLine){
+  var lineCurrent = [];
   if (katzDeliLine.length === 0){
-    return "The line is currently empty.";
-  }
+    return "The line is currently empty.";}
   else {
-    var output = [];
-    for (let i = 0; i < katzDeliLine.length; i++){
-      output.push (`${[i+1]}. ${katzDeliLine[i]}`);
-    }
-    return `The line is currently: ${output.join(", ")}`;
-    }
+    for (let i = 0; i < katzDeliLine.length; i++)
+      lineCurrent.push(`${i+1}. ${katzDeliLine[i]}`);
+  }
+  return `The line is currently: ${lineCurrent.join (", ")}`;
 }
